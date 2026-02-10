baea906ed1544848c54cff12b483685c6d56eae5
user <Saveli Platonov>
2026-02-10 19:56:02 +0300

    client_cli error handling

--- client_cli.py
+++ client_cli.py
@@ -23,45 +23,45 @@ if __name__ == "__main__":
 
     logger = logging.getLogger(__name__)
 
+    logger.addHandler(lambda data: print(data))
+
     client = FileStorageClient(logger)
 
     login = args.login
     if not login:
-        login ='anonymous'
+        login = 'anonymous'
+
     password = args.password
     if not password:
         password = 'anonymous'
 
-    client.authenticate(login, password)
-
-    command = args.command
-
-    match command:
-        case 'user_rename':
-            client.rename_user(args.new_username)
-
-        case 'list':
-            res = client.list_files()
-            print(res)
-
-        case 'remove_file':
-            client.remove_file(args.file_name)
-
-        case 'rename_file':
-            client.rename_file(args.file_name, args.new_file_name)
-
-        case 'upload_file':
-            client.upload_file(args.file_name)
-
-        case 'update_file':
-            client.update_file(args.file_name)
-
     try:
-        bytes_sent = FileStorageClient.upload_file(args.server, args.file_name)
-        print("Upload succeeded, bytes sent: {bytes_sent}")
+
+        res = client.connect(args.server)
+
+        client.authenticate(login, password)
+
+        command = args.command
+
+        match command:
+            case 'user_rename':
+                client.rename_user(args.new_username)
+
+            case 'list':
+                res = client.list_files()
+                print(res)
+
+            case 'remove_file':
+                client.remove_file(args.file_name)
+
+            case 'rename_file':
+                client.rename_file(args.file_name, args.new_file_name)
+
+            case 'upload_file':
+                client.upload_file(args.file_name)
+
+            case 'update_file':
+                client.update_file(args.file_name)
 
     except Exception as ex:
-        print("Some exception raised", ex.with_traceback)
-
-    finally:
-        print("Exiting cli")
+        print("Some exception happened, exiting cli...")
